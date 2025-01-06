0ac967582edb9fcfb4f2399d32a77822a52b7d4f
Yeogibeom <gibeom250@gmail.com>
2025-01-06 15:19:37 +0900

    우리 bas 로그인화면

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <UserLogin /> <!-- 변경된 컴포넌트 이름 사용 -->
+  </div>
+</template>
+
+<script>
+import UserLogin from './components/Login.vue'; // 변경된 컴포넌트 이름
+
+export default {
+  name: 'App',
+  components: {
+    UserLogin, // 등록된 이름도 수정
+  },
+};
+</script>
