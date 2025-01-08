8f09c0b45b92b242342da8572ce02349102cc695
Yeogibeom <gibeom250@gmail.com>
2025-01-08 10:51:18 +0900

    관리자 ,일반사용자 권한 부여 후 vue페이지 부트스트랩 적용

--- src/main/java/example/wooribastest/controller/LoginController.java
+++ src/main/java/example/wooribastest/controller/LoginController.java
@@ -27,23 +27,23 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody UserForm dto, HttpServletResponse response) throws Exception {
-      boolean loginSuccess = userService.login(dto);
-      if (loginSuccess) {
-          String role = userService.getRole(dto);
-          // 역할이 null이 아니고, 로그인 정보가 일치하면
-          if (role != null) {
-              // 역할에 따라 처리
-              if ("ADMIN".equals(role)) {
-                  return ResponseEntity.ok("관리자 로그인");
-              } else if ("ADVANCED_USER".equals(role)) {
-                  return ResponseEntity.ok("고급사용자 로그인");
-              } else if ("USER".equals(role)) {
-                  return ResponseEntity.ok("일반 사용자 로그인");
-              } else {
-                  return ResponseEntity.status(401).body("Login Failed: Unknown Role");
-              }
-          }
-      }
+        boolean loginSuccess = userService.login(dto);
+        if (loginSuccess) {
+            String role = userService.getRole(dto);
+            // 역할이 null이 아니고, 로그인 정보가 일치하면
+            if (role != null) {
+                // 역할에 따라 처리
+                if ("ADMIN".equals(role)) {
+                    return ResponseEntity.ok("관리자 로그인");
+                } else if ("ADVANCED_USER".equals(role)) {
+                    return ResponseEntity.ok("고급사용자 로그인");
+                } else if ("USER".equals(role)) {
+                    return ResponseEntity.ok("일반 사용자 로그인");
+                } else {
+                    return ResponseEntity.status(401).body("Login Failed: Unknown Role");
+                }
+            }
+        }
         // 사용자 정보가 없거나 비밀번호가 틀린 경우
         return ResponseEntity.status(401).body("Login Failed: Invalid Credentials");
 
